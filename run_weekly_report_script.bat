0767a2d7c0df69b70adc44dd47bb4eab03f797bb
Kelly Adams <81118926+kellyjadams@users.noreply.github.com>
2024-06-12 13:36:20 -0700

    Update run_weekly_report_script.bat

--- run_weekly_report_script.bat
+++ run_weekly_report_script.bat
@@ -1,4 +1,4 @@
 @echo off
 CALL C:\Users\user_name\anaconda3\Scripts\activate.bat C:\Users\user_name\anaconda3
 CALL conda activate venv1
-python C:\Users\user_name\my_file_path\weekly_report.py > C:\Users\user_name\my_file_path\Logging\output.log 2>&1
+python C:\Users\user_name\my_file_path\weekly_sales_report_automatic.py > C:\Users\user_name\my_file_path\Logging\output.log 2>&1
